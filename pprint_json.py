c15a695f80be690d0842603955434b5a7fadfc3c
AMSolovyev <alexander.solovyev7@gmail.com>
2018-05-25 20:22:12 +0300

    I have corrected the name

--- pprint_json.py
+++ pprint_json.py
@@ -6,7 +6,7 @@ DEFAULT_FILE_NAME = 'alco_shops.json'
 DEFAULT_FILE_ENCODING = 'utf-8'
 DEFAULT_INDENT = 4
 
-def load_from_json(filepath, file_encoding):
+def load_data_from_json(filepath, file_encoding):
         with open(filepath, 'r', encoding=file_encoding) as file_handler:
             return json.load(file_handler)
 
@@ -26,5 +26,5 @@ if __name__== '__main__':
         print('Файл {} не существует'.format(work_file))
         sys.exit(1)
 
-    data_from_json = load_from_json(work_file, DEFAULT_FILE_ENCODING)
+    data_from_json = load_data_from_json(work_file, DEFAULT_FILE_ENCODING)
     print(printer_json(data_from_json))
